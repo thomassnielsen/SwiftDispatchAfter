b7321dd67e72682911d831c9361dd1b2873bc774
thomassnielsen <me@thomassnielsen.com>
2017-05-03 11:43:43 +0200

    Upgrade to swift 3

--- Classes/DispatchAfter.swift
+++ Classes/DispatchAfter.swift
@@ -9,33 +9,33 @@
 
 import Foundation
 
-public func delay(aDelay:NSTimeInterval, closure: () -> Void) {
-    
-    delay(aDelay, dispatch_get_main_queue(), closure)
-    
+public func delay(_ aDelay:TimeInterval, closure: @escaping () -> Void) {
+
+    delay(aDelay, queue: DispatchQueue.main, closure: closure)
+
 }
 
-public func delay(aDelay:NSTimeInterval, queue: dispatch_queue_t!, closure: () -> Void) {
-    
-    let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(aDelay * Double(NSEC_PER_SEC)))
-    dispatch_after(delayTime, queue, closure)
-    
+public func delay(_ aDelay:TimeInterval, queue: DispatchQueue, closure: @escaping () -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + aDelay) {
+        closure()
+    }
+
 }
 
 public extension Int {
-    var second: NSTimeInterval { return NSTimeInterval(self) }
-    var seconds: NSTimeInterval { return NSTimeInterval(self) }
-    var minute: NSTimeInterval { return NSTimeInterval(self * 60) }
-    var minutes: NSTimeInterval { return NSTimeInterval(self * 60) }
-    var hour: NSTimeInterval { return NSTimeInterval(self * 3600) }
-    var hours: NSTimeInterval { return NSTimeInterval(self * 3600) }
+    var second: TimeInterval { return TimeInterval(self) }
+    var seconds: TimeInterval { return TimeInterval(self) }
+    var minute: TimeInterval { return TimeInterval(self * 60) }
+    var minutes: TimeInterval { return TimeInterval(self * 60) }
+    var hour: TimeInterval { return TimeInterval(self * 3600) }
+    var hours: TimeInterval { return TimeInterval(self * 3600) }
 }
 
 public extension Double {
-    var second: NSTimeInterval { return self }
-    var seconds: NSTimeInterval { return self }
-    var minute: NSTimeInterval { return self * 60 }
-    var minutes: NSTimeInterval { return self * 60 }
-    var hour: NSTimeInterval { return self * 3600 }
-    var hours: NSTimeInterval { return self * 3600 }
+    var second: TimeInterval { return self }
+    var seconds: TimeInterval { return self }
+    var minute: TimeInterval { return self * 60 }
+    var minutes: TimeInterval { return self * 60 }
+    var hour: TimeInterval { return self * 3600 }
+    var hours: TimeInterval { return self * 3600 }
 }
